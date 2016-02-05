a310f6420a29cce17d48e65e3439c8bb877774f4
Alexiron <alexironr@gmail.com>
2016-02-05 18:06:21 +0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
 
-  resources :users, except:    [:new]
-  resources :sessions, only:   [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users, except:    [:new] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions, only:      [:new, :create, :destroy]
+  resources :microposts, only:    [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   #get 'user/:id' => 'users#show', as: :user
   #post 'user/:id/create' => 'users#create'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,12 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   it { should be_valid }
   it { should_not be_admin}
@@ -27,6 +33,7 @@ describe User do
 
     it { should be_admin }
   end
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -144,6 +151,75 @@ describe User do
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+
+  describe "relationships associations" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+      other_user.follow!(@user)
+    end
+
+    it "should destroy associated relationships" do
+      followers = @user.followers.to_a
+      @user.destroy
+      expect(followers).not_to be_empty
+      followers.each do |user|
+        expect(Relationship.where(followed_id: user.id)).to be_empty
+      end
+    end
+    it "should destroy associated reverse relationships" do
+      followed_users = @user.followed_users.to_a
+      @user.destroy
+      expect(followed_users).not_to be_empty
+      followed_users.each do |user|
+        expect(Relationship.where(follower_id: user.id)).to be_empty
+      end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
